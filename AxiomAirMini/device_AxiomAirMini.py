0dde9bc327b677828afd21ea9af1130ad879ab2b
Ryan McQuen <rpcm@linux.com>
2020-10-16 08:54:43 -0700

    Add support for mixer controls on knobs 1-8.

--- AxiomAirMini/device_AxiomAirMini.py
+++ AxiomAirMini/device_AxiomAirMini.py
@@ -1,8 +1,21 @@
 # name=Axiom Air Mini
 # url=https://github.com/ryanpcmcquen/fl_studio_scripting
 
+#
+# Author: Ryan McQuen
+#
+# Changelog:
+# ----------
+#
+# 0.1.0 - October 15 2020
+# Initial release. Add support for transport and ui controls.
+
+# 0.2.0 - October 16 2020
+# Add support for mixer controls on knobs 1-8.
+
 
 import midi
+import mixer
 import transport
 import ui
 
@@ -14,8 +27,19 @@ Buttons = {
     'Down': 0x14,
     'Right': 0x15,
     'Left': 0x16,
-    'Center': 0x17
+    'Center': 0x17,
 }
+
+Knobs = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+]
 
 
 def OnMidiMsg(event):
@@ -23,8 +47,10 @@ def OnMidiMsg(event):
     # It's then passed onto FL Studio to use.
     event.handled = False
 
-    # Prints the data recieved to the 'Script output' window (for debugging).
-    # print(event.midiId, event.data1, event.data2)
+    # Prints the data recieved to the 'Script output' window (for debugging):
+    print(event.midiId, event.data1, event.data2)
+    # Prints the whole kit and kaboodle:
+    print(event)
 
     # Use midi.MIDI_NOTEON for note events.
     if event.midiId == midi.MIDI_CONTROLCHANGE:
@@ -53,4 +79,9 @@ def OnMidiMsg(event):
                 event.handled = True
             elif event.data1 == Buttons['Center']:
                 ui.enter()
+                event.handled = True
+
+            elif event.data1 in Knobs:
+                mixer.setTrackVolume(
+                    event.data1, event.data2 / 100)
                 event.handled = True
